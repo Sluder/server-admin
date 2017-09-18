576b7f2c8692d3cc7f4652597edda3fac4016614
zachary.sluder@gmail.com <vagrant@homestead>
2017-09-18 03:03:08 +0000

    Theme change; Website added

---
+++ database/migrations/2017_09_18_005039_create_websites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWebsitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 15);
+            $table->string('domain', 25);
+            $table->boolean('display')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('websites');
+    }
+}
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -19,11 +19,21 @@
                 <div class="col-md-2">
                     <div class="navbar navbar-fixed-left">
                         <div class="header">
-                            <p><i class="fa fa-cogs" aria-hidden="true"></i> Admin Panel</p>
+                            <p><i class="fa fa-cogs" aria-hidden="true"></i> <a href="{{ route('home') }}">Admin Panel</a></p>
                         </div>
                         <ul class="nav navbar-nav">
                             <li>
                                 <a href="{{ route('server') }}"><i class="fa fa-bars" aria-hidden="true"></i> Hosting Server</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('server') }}"><i class="fa fa-desktop" aria-hidden="true"></i> Websites</a>
+                                <ul class="website-list">
+                                    @forelse (\App\Website::all() as $website)
+                                        <a href="" class="website"><li>Music</li></a>
+                                    @empty
+                                        <div class="website empty"><li>No websites to show</li></div>
+                                    @endforelse
+                                </ul>
                             </li>
                         </ul>
                     </div>
